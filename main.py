9d73e44e4eef1f059baabf8f4904f96c4ead7f62
Dylan Le <68560708+ExoticButters15@users.noreply.github.com>
2025-04-09 16:14:59 -0700

    put it in main for future function development

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ from google import genai
 from dotenv import find_dotenv, load_dotenv
 import os
 
-env_path = find_dotenv()
-load_dotenv(env_path)
-gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-client = genai.Client(api_key=gemini_api_key)
+def main():
+    env_path = find_dotenv()
+    load_dotenv(env_path)
+    gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents="Is vanilla and tarragon a good flavor combination for butter poached lobster?",
-)
+    client = genai.Client(api_key=gemini_api_key)
 
-print(response.text)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents="Is vanilla and tarragon a good flavor combination for butter poached lobster?",
+    )
+
+    print(response.text)
+
+
+main()
