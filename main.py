3d0994d2fe1183d1c6ed34007a1a1e6f6123477c
Dylan Le <68560708+ExoticButters15@users.noreply.github.com>
2025-04-09 16:51:03 -0700

    prompt builder with chunk list

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from google import genai
 from dotenv import find_dotenv, load_dotenv
 import os
 
+resources = ["tarragon and vanilla is disgusting",
+             "tarragon does not taste good with lobster",
+             "vanilla does not emulsify with butter well"]
+
+def prompt_builder(chunks: list[str], user_prompt: str) -> str:
+    context = "\n".join(chunks)
+
+    return (
+        f"Your job is to evaluate the flavor compatibility of ingredients."
+        f"Do NOT include phrases like based on the context according to the above."
+        f"Respond with a confident, well-structured answer."
+        f"Context:\n{context}\n"
+        f"Question:\n{user_prompt}\n"
+    )
 
 def main():
     env_path = find_dotenv()
@@ -12,10 +26,9 @@ def main():
 
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents="Is vanilla and tarragon a good flavor combination for butter poached lobster?",
+        contents=prompt_builder(resources, "Is vanilla and tarragon a good flavor combination for butter poached lobster?"),
     )
 
     print(response.text)
-
 
 main()
