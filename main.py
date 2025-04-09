c83b1e6815c50686bd5d4f5dc52ba622827ec61f
Dylan Le <68560708+ExoticButters15@users.noreply.github.com>
2025-04-09 15:54:59 -0700

    api call sorted out interpreter

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("ai-integration")
+from google import genai
+from dotenv import find_dotenv, load_dotenv
+import os
+
+env_path = find_dotenv()
+load_dotenv(env_path)
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=gemini_api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="Is vanilla and tarragon a good flavor combination for butter poached lobster?",
+)
+
+print(response.text)
